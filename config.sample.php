c40bca70f6617e5b64dda08c0d7105073a0b7ee3
Josua Mayer <privacy@not.given>
2016-02-20 10:32:25 +0100

    mysql config: make hostname, port; or hostname, socket one single field

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,26 @@
 <?php
+/**
+ * PillBox
+ * Copyright (C) 2016 Sindastra <sindastra@gmail.com>
+ * Copyright (C) 2016 Artox <privacy@not.given>
+ *
+ * The above copyright notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 $mysqlc = array(
-		'host' => 'localhost',
-		'port' => 3306,
+		'server' => 'localhost:3306',
+		// 'server' => 'localhost:/var/run/mysql/mysql.sock',
 		'username' => '',
 		'password' => '',
 		'database' => ''
 	);
+
+?>
