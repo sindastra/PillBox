cfbef6d83e18bff6e5be2c7f86e1f6627e6a44c8
Josua Mayer <privacy@not.given>
2016-02-20 19:23:38 +0100

    implement store new medication

--- store.php
+++ store.php
@@ -27,7 +27,21 @@
  * Valid types are:
  * MEDICATION_ADD: add new medication information
  * parameters:
- * TODO
+ * name
+ * active_agent
+ * dosage_package
+ * dosage_package_unit
+ * dosage_to_take
+ * dosage_to_take_unit
+ * colour
+ * shape
+ * food_instructions
+ * indication
+ * minimum_spacing
+ * minimum_spacing_unit
+ * maximum_dosage
+ * maximum_dosage_unit
+ * note
  * 
  * MEDICATION_LOG: log taking or not taking medication
  * parameters:
@@ -90,6 +104,44 @@ if($data == NULL) {
 
 // execute
 switch($data->type) {
+	case StoreType::MEDICATION_ADD:
+		/*
+		 * expected fields: name, active_agent, dosage_package, dosage_package_unit, dosage_to_take, dosage_to_take_unit, colour
+		 * shape, food_instructions, indication, minimum_spacing, minimum_spacing_unit, maximum_dosage, maximum_dosage_unit, note
+		 * TODO: schedule, schedule_id
+		 */
+		if( !isset($data->name) || !isset($data->active_agent) || !isset($data->dosage_package)
+		    || !isset($data->dosage_package_unit) || !isset($data->dosage_to_take) || !isset($data->dosage_to_take_unit)
+		    || !isset($data->colour) || !isset($data->shape) || !isset($data->food_instructions)
+		    || !isset($data->indication) || !isset($data->minimum_spacing) || !isset($data->minimum_spacing_unit)
+		    || !isset($data->maximum_dosage) || !isset($data->maximum_dosage_unit) || !isset($data->note) ) {
+			$result['error'] = 'Missing data!';
+			break;
+		}
+		// TODO: check for valid data
+
+		// store
+		$query = sprintf('INSERT INTO `medications` (`user_id`, `name`, `active_agent`, `dosage_package`, `dosage_package_unit`, `dosage_to_take`, `dosage_to_take_unit`, `colour`, `shape`, `food_instructions`, `indication`, `minimum_spacing`, `minimum_spacing_unit`, `maximum_dosage`, `maximum_dosage_unit`, `note`, `time`) VALUES (%u, "%s", "%s", %F, "%s", %F, "%s", %u, %u, %u, "%s", %F, "%s", %F, "%s", "%s", NOW())',
+		    $data->name, $data->active_agent, $data->dosage_package,
+		    $data->dosage_package_unit, $data->dosage_to_take, $data->dosage_to_take_unit,
+		    $data->colour, $data->shape, $data->food_instructions,
+		    $data->indication, $data->minimum_spacing, $data->minimum_spacing_unit,
+		    $data->maximum_dosage, $data->maximum_dosage_unit, $data->note);
+		if(mysql_query($query, $mysql) == FALSE) {
+			$result['error'] = 'Query ' . $query . ' failed: ' . mysql_error($mysql);
+			break;
+		}
+
+		// get ID of new medication entry
+		$id = mysql_insert_id($mysql);
+		if($id == FALSE || $id == 0) {
+			$result['error'] = "An unexpected error occured!";
+			break;
+		}
+
+		// return ID as proof of success
+		$result['id'] = $id;
+		break;
 	case StoreType::MEDICATION_LOG:
 		// expected fields: medication_id, quantity, timestamp, status, note (may be empty)
 		if(!isset($data->medication_id) || !isset($data->quantity) || !isset($data->timestamp) || !isset($data->status) || !isset($data->note)) {
@@ -116,7 +168,7 @@ switch($data->type) {
 			break;
 		}
 
-		// get ID of new log Log entry
+		// get ID of new Log entry
 		$id = mysql_insert_id($mysql);
 		if($id == FALSE || $id == 0) {
 			$result['error'] = "An unexpected error occured!";
