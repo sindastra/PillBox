85c3b63433adbe9f3cc9c4f8d286fc3399f674b8
Josua Mayer <privacy@not.given>
2016-02-20 14:22:09 +0100

    extend protcol for store.php: standardized result

--- store.php
+++ store.php
@@ -25,6 +25,9 @@
  * { type = "add_medication", TODO }
  * { type = "add_schedule????", TODO }
  * TODO
+ * There will be a reply with an ID and an error string
+ * non-zero ID means success, and error will be empty
+ * 
  */
 
 // define constants
@@ -48,51 +51,66 @@ include "include/mysql_open_database.inc";
 
 include "functions.php";
 
+// initialize a dummy failed result
+$result = array('id' => 0, 'error' => '');
+
 // check if user is logged in
 if(!is_logged_in()) {
-	die("Please log in first!");
+	$result['error'] = 'Please log in first!';
+	echo json_encode($result);
+	exit(0);
 }
 
 // check if data was passed
 if(!array_key_exists('json', $_POST)) {
-	die("Please append data!");
+	$result['error'] = 'Please append data!';
+	echo json_encode($result);
+	exit(0);
 }
 
 // try to decode
 $data = json_decode($_POST['json']);
-if($data == NULL)
-	die("Failed to decode json!");
+if($data == NULL) {
+	$result['error'] = 'Failed to decode json!';
+	echo json_encode($result);
+	exit(0);
+}
 
 // execute
 switch($data->type) {
 	case StoreType::LOG:
 		// expected fields: gid, timestamp, status
-		if(empty($data->gid) || empty($data->timestamp) || empty($data->status))
-			die("Missing data!");
+		if(empty($data->gid) || empty($data->timestamp) || empty($data->status)) {
+			
+			$result['error'] = 'Missing data!';
+			break 2;
+		}
 
 		// TODO: check validity
 
 		// store
 		$query = sprintf('INSERT INTO `log` (`medication_id`, `timestamp`, `status`) VALUES (%u, FROM_UNIXTIME(%u), %u)', $data->gid, $data->timestamp, $data->status);
 		if(mysql_query($query, $mysql) == FALSE) {
-			die($query . ': ' . mysql_error($mysql));
+			$result['error'] = 'Query ' . $query . ' failed: ' . mysql_error($mysql);
+			break 2;
 		}
 
 		// get ID of new log Log entry
 		$id = mysql_insert_id($mysql);
 		if($id == FALSE || $id == 0) {
-			die("An unexpected error occured!");
+			$result['error'] = "An unexpected error occured!";
+			break 2;
 		}
 
 		// return ID as proof of success
-		$result = array('id' => $id);
-		$result = json_encode($result);
+		$result['id'] = $id;
 		break;
 	case StoreType::MED:
 		break;
 	default;
-		die("Invalid type!");
+		$result['error'] = 'Invalid type!';
+		break;
 }
 
 // return the result
-echo $result;
+echo json_encode($result);
