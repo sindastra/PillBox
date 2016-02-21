ad8c540617e6b9ccf8da6e7dd6f6e635d7fa6be0
Sindastra <sindastra+github@gmail.com>
2016-02-21 14:27:55 +0100

    Added main.js with experimental functions.

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var requestURL = "request.php";
+var storeURL   = "store.php";
+
+function get_all_medications()
+{
+    $.post(requestURL,{json:'{"type":"MEDICATIONS_GET"}'}, function(data){
+        data["data"].forEach(function(entry){
+            console.log(entry["name"]);
+        });
+    }, "json");
+}
+
+function create_medication(name,dosage_package,dosage_package_unit,
+                            active_agent,dosage_to_take,dosage_to_take_unit,
+                           colour,shape,food_instructions,indication,
+                           minimum_spacing,minimum_spacing,
+                            minimum_spacing_unit,maximum_dosage,
+                            maximum_dosage_unit,note)
+{
+    var medication_data = {
+        "type":"1",
+        "name":name,
+        "dosage_package":dosage_package,
+        "dosage_package_unit":dosage_package_unit,
+        "active_agent":active_agent,
+        "dosage_to_take":dosage_to_take,
+        "dosage_to_take_unit":dosage_to_take_unit,
+        "colour":colour,
+        "shape":shape,
+        "food_instructions":food_instructions,
+        "indication":indication,
+        "minimum_spacing":minimum_spacing,
+        "minimum_spacing_unit":minimum_spacing_unit,
+        "maximum_dosage":maximum_dosage,
+        "maximum_dosage_unit":maximum_dosage_unit,
+        "note":note
+    }
+    
+    $.post(storeURL, {json:medication_data}, function(data){
+        console.log(data);
+    },"json");
+}
