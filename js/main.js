baed8a460c237a67876a23cae0ddb8752dc22513
Sindastra <sindastra+github@gmail.com>
2016-02-22 07:08:04 +0100

    Added license headers.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/**
+ * PillBox
+ * Copyright (C) 2016 Sindastra <sindastra@gmail.com>
+ * Copyright (C) 2016 Artox <privacy@not.given>
+ *
+ * The above copyright notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 var requestURL = "load.php";
 var storeURL   = "store.php";
 var deleteURL  = "delete.php";
